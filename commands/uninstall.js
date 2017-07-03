f5d6b09fa3c685542f0cd92796b09bb3749b0ebe
Bill Hunt <bill@krues8dr.com>
2017-07-03 14:00:09 -0400

    Add uninstall command.
    To solve the problems in #6, we're prepending the filename with the mod name and using glob to match.

--- commands/uninstall.js
+++ commands/uninstall.js
@@ -1,35 +1,26 @@
 var CurseforgeRepository = require('../lib/curseforgeRepository');
 
-module.exports = function installCommand(program) {
+module.exports = function uninstallCommand(program) {
   'use strict';
 
   program
-    .command('install [mods ...]')
-    .description('Install one or more mods.')
+    .command('uninstall <mods...>')
+    .description('Uninstall one or more mods.')
     .action(function(args, command) {
       // Merge our command line arguments to override our config file.
       let options = Object.assign(program.conf,
         // Remove undefined options using the JSON twiddle.
         JSON.parse(JSON.stringify(program.opts())));
 
-      let mods = {};
-      if(args && args.length > 0) {
-        let modversion = options.modversion || '*';
-        for(let arg of args) {
-          mods[arg] = modversion;
-        }
-      }
-      else if(program.conf.mods) {
-        mods = program.conf.mods;
-      }
+      let mods = args;
 
-      if(mods && Object.keys(mods).length > 0) {
+      if(mods && mods.length > 0) {
         let repo = new CurseforgeRepository(options);
         repo.setup().then( () => {
-          for(let [mod, version] of Object.entries(mods)) {
-            repo.get(mod, version)
+          for(let mod of mods) {
+            repo.remove(mod)
               .then(() => {
-                console.log(`Installed ${mod}`);
+                console.log(`Unistalled ${mod}`);
               })
               .catch((err) => {
                 console.warn(err);
