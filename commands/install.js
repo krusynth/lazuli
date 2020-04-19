bab3bf564a428c7bcfb19e0122518b24f4c9c27c
Bill Hunt <bill@krues8dr.com>
2020-04-19 15:59:47 -0400

    Add new browser-based engine for downloading to workaround Cloudflare. Refactor library.

--- commands/install.js
+++ commands/install.js
@@ -10,27 +10,30 @@ module.exports = function installCommand(program) {
     .description('Install one or more mods.')
     .action(install);
 
-  function install(args, command) {
+  async function install(args, command) {
     let options = Config(program);
     let installer = new Install(options);
+    await installer.init();
 
-    let mods = {};
-    if(args && args.length > 0) {
-      for(let arg of args) {
-        let [name, modversion] = arg.split('@');
-        modversion = modversion || '*';
-        mods[name] = modversion;
+      let mods = {};
+      if(args && args.length > 0) {
+        for(let arg of args) {
+          let [name, modversion] = arg.split('@');
+          modversion = modversion || '*';
+          mods[name] = modversion;
+        }
       }
-    }
-    else if(program.conf.mods) {
-      mods = program.conf.mods;
-    }
+      else if(program.conf.mods) {
+        mods = program.conf.mods;
+      }
 
-    if(mods && (!args || args.length === 0)) {
-      // If we're installing from scratch, remove the old lockfile.
-      installer.clearLockfile();
-    }
+      if(mods && (!args || args.length === 0)) {
+        // If we're installing from scratch, remove the old lockfile.
+        installer.clear();
+      }
 
-    installer.run(mods);
+      await installer.run(mods);
+      await installer.shutdown();
+      return true;
   }
 };
--- commands/uninstall.js
+++ commands/uninstall.js
@@ -1,33 +1,26 @@
+'use strict';
 
+const Config = require('../lib/config');
+const Uninstall = require('../lib/uninstall');
 
 module.exports = function uninstallCommand(program) {
-  'use strict';
 
   program
-    .command('uninstall <mods...>')
+    .command('uninstall [mods...]')
     .description('Uninstall one or more mods.')
-    .action(function(args, command) {
-      // Merge our command line arguments to override our config file.
-      let options = Object.assign(program.conf,
-        // Remove undefined options using the JSON twiddle.
-        JSON.parse(JSON.stringify(program.opts())));
+    .action(uninstall);
 
-      let mods = args;
-
-      if(mods && mods.length > 0) {
-        let repo = new CurseforgeRepository(options);
-        repo.setup().then( () => {
-          for(let mod of mods) {
-            repo.remove(mod)
-              .then(() => {
-                console.log(`Unistalled ${mod}`);
-              })
-              .catch((err) => {
-                console.warn(err);
-              });
-          }
-        });
-      }
-    });
-
+  function uninstall(args, command) {
+    let options = Config(program);
+    let uninstaller = new Uninstall(options);
+    return uninstaller.init()
+    .then(() => uninstaller.run(args))
+    .then(files => {
+      files.forEach(file => console.log(`Removed ${file}`.green));
+    })
+    .catch(e => {
+      console.warn(e.red.bold);
+    })
+    .finally(() => uninstaller.shutdown());
+  }
 };
